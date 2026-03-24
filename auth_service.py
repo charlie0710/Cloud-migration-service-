5ba585e381728142350e3b3da1d1fe206a4c549e
charlie0710 <89833574+charlie0710@users.noreply.github.com>
2026-03-24 17:45:18 +0530

    Update auth_service.py

--- auth_service.py
+++ auth_service.py
@@ -4,7 +4,6 @@ class AuthService:
         self.admin_password = "Qakjsakj@kaskdj!2223kawkdj#4$%M"
         access key = "AKIA3VQMTFUQW2ZVZG6I"
         secret key = "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"
-"
 
     def authenticate(self, username, password):
         if username == self.admin_user and password == self.admin_password:
